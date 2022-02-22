0125c06bd5b3e04c0e812864485ad64564277268
NejcDr <nejcdr02@gmail.com>
2022-02-22 18:03:43 +0100

    .

--- Gravitacija.java
+++ Gravitacija.java
@@ -4,4 +4,12 @@ public class Gravitacija
     {
         System.out.println("OIS je zakon!");
     }
+	
+	public double Izracun (double v) {
+		double G = 6.674 * Math.pow(10, -11);
+		double M = 5.972 * Math.pow(10, 24);
+		double r = 6.371 * Math.pow(10, 6);
+		
+		return (G*M) / Math.pow(r + v, 2);
+	}
 }
