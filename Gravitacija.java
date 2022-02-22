f10590f81b07c1ec62b66f3943a080f42f3fae7c
VoidRune <mhbernik@gmail.com>
2022-02-22 19:02:22 +0100

    master

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,21 +1,27 @@
+import java.util.Scanner;
+
 public class Gravitacija
 {
     public static void main(String[] args)
     {
-        System.out.println("OIS je zakon!");
+        Scanner sc = new Scanner(System.in);
+        double nadmorskaVisina = sc.nextDouble();
+        Izpis(nadmorskaVisina);
     }
 	
-	public static double Izracun (double v) {
+    //nadmorska visina je v kilometrih je v kilometrih
+	public static double Izracun (double v)
+    {
 		double G = 6.674 * Math.pow(10, -11);
 		double M = 5.972 * Math.pow(10, 24);
 		double r = 6.371 * Math.pow(10, 6);
 		
-		return (G*M) / Math.pow(r + v, 2);
+		return (G*M) / Math.pow(r + v * 1000.0, 2);
 	}
 
     public static void Izpis(double v)
     {
-        System.out.printf("Nadmorska višina: %f", v);
+        System.out.printf("Nadmorska višina: %f\n", v);
         System.out.printf("Gravitacijski pospešek: %f ", Izracun(v));
     }
 
